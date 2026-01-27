cf892fe5881b0dd8c4537df8a6288d7c72b489d5
jishan Panigrahi <jishanpanigrahi@gmail.com>
2026-01-28 00:29:51 +0530

    Added /data/boardTiles.js

---
+++ src/app/data/boardTiles.js
@@ -0,0 +1,17 @@
+export const SPECIAL_TILES = {
+  // Doors (Ladders)
+  3:  { type: "door", to: 17 },
+  9:  { type: "door", to: 31 },
+  21: { type: "door", to: 32 },
+  44: { type: "door", to: 92 },
+  69: { type: "door", to: 88 },
+
+  // Time Rifts (Snakes / Past Time Collapse)
+  9: { type: "rift", to: 2 },
+  26: { type: "rift", to: 8 },
+  50: { type: "rift", to: 35 },
+  63: { type: "rift", to: 41 },
+  71: { type: "rift", to: 55 },
+  88: { type: "rift", to: 67 },
+  97: { type: "rift", to: 78 },
+};
